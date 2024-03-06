964ecee09ddd5efc1cbdab9a488af1b4f9149d92
Pradiptaa <diptaarya18@gmail.com>
2024-03-06 15:05:12 +0700

    [REFACTOR] Apply OrderStatus enum to Order model tests.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,5 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.model;
-
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
