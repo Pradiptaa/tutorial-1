41865551d9d192cbd93d880a4985c7ca51b3f9ed
Pradiptaa <diptaarya18@gmail.com>
2024-03-06 16:18:43 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -3,10 +3,11 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface OrderService {
     public Order createOrder(Order order);
     public Order updateStatus(String orderId, String status);
     public Order findById(String orderId);
-    public List<Order> findByAuthor(String author);
+    public List<Order> findAllByAuthor(String author);
 }
